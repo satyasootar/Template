e792c89dff9aedcfc7ed8c9cd6424fe08901451f
satyasootar <satya.sootar06@gmail.com>
2025-03-19 20:37:32 +0530

    add Navbar1 component, implement UI components for form elements, and update package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ChatBot from "./components/ChatBot"
+import { Navbar1 } from "./components/Navbar1"
 
 
 const App = () => {
   return (
-    <div>
+    <div className="px-10" >
+      <Navbar1 />
       <ChatBot />
 
     </div>
