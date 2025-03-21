5488a66b354ac86f9c78b4d2eb80058fe7e7c25b
satyasootar <satya.sootar06@gmail.com>
2025-03-21 11:42:53 +0530

    Add WhyUS component and integrate feature steps; update package.json and package-lock.json for @tabler/icons-react

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Footer1 } from "./components/Footer"
 import { Navbar } from "./components/Navbar"
 import SignupForm from "./components/SignUp"
 import { DemoHeroGeometric } from "./components/Hero"
-
+import { WhyUS } from "./components/WhyUS"
 
 const App = () => {
   return (
     <div >
       <Navbar />
       <DemoHeroGeometric />
+      <WhyUS />
       <Footer1 />
       <ChatBot />
 
